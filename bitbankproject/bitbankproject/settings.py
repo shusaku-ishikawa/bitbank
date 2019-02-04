ff3222816ecc17dbcdc12807c8fab9e958a97aa0
root <ishikawasyuusaku@gmail.com>
2019-02-04 13:12:34 +0900

    a

--- bitbankproject/bitbankproject/settings.py
+++ bitbankproject/bitbankproject/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'bitbank.User'
 LOGIN_URL = 'bitbank:login'
-LOGIN_REDIRECT_URL = 'bitbank:main'
+LOGIN_REDIRECT_URL = 'bitbank:order'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
