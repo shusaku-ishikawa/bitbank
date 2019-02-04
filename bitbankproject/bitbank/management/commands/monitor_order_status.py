390a9d7f7df93a48a4880b1bfbfdd01bd33a73cb
shusaku-ishikawa <ishikawasyuusaku@gmail.com>
2019-02-04 15:09:59 +0900

    a

--- bitbankproject/bitbank/management/commands/monitor_order_status.py
+++ bitbankproject/bitbank/management/commands/monitor_order_status.py
@@ -24,54 +24,55 @@ class Command(BaseCommand):
     # コマンドが実行された際に呼ばれるメソッド
     def handle(self, *args, **options):
         logger = logging.getLogger('batch_logger')
-        logger.info("started")
-        for user in User.objects.all():
-            # API KEYが登録されているユーザのみ処理
-            if user.api_key != "" or user.api_secret_key != "":
-                # キー情報セット
-                try:
-                    prv = python_bitbankcc.private(user.api_key, user.api_secret_key)
-                except Exception as e:
-                    logger.error('User:' + user.email + ' Message: ' +  e.args)
-                    continue
+        for i in range(10):
+            logger.info("started")
+            for user in User.objects.all():
+                # API KEYが登録されているユーザのみ処理
+                if user.api_key != "" or user.api_secret_key != "":
+                    # キー情報セット
+                    try:
+                        prv = python_bitbankcc.private(user.api_key, user.api_secret_key)
+                    except Exception as e:
+                        logger.error('User:' + user.email + ' Message: ' +  e.args)
+                        continue
 
-                # 通貨ペアをイテレーション
-                for pair in Order.PAIR:
-                    orders_by_pair = Order.objects.filter(pair=pair).filter(status__in=['UNFILLED', 'PARTIALLY_FILLED']).exclude(order_id__isnull=True).values_list('order_id', flat=True)
-                    # オーダが存在する通貨のみ処理
-                
-                    if orders_by_pair.count() != 0:
-                        try:
-                            ret = prv.get_orders_info(
-                                pair, # ペア
-                                list(orders_by_pair) # 注文IDのリスト
-                            )
-                        except Exception as e:
-                            logger.error('user:' + user.email + ' pair: ' + pair + ' order id: ' + str(orders_by_pair) + ' error: ' + str(e.args))
-                            continue
-
-                        for order_dict in ret.get('orders'):
+                    # 通貨ペアをイテレーション
+                    for pair in Order.PAIR:
+                        orders_by_pair = Order.objects.filter(pair=pair).filter(status__in=['UNFILLED', 'PARTIALLY_FILLED']).exclude(order_id__isnull=True).values_list('order_id', flat=True)
+                        # オーダが存在する通貨のみ処理
+                    
+                        if orders_by_pair.count() != 0:
                             try:
-                                order_id = order_dict.get('order_id')
-                                remaining_amount = order_dict.get('remaining_amount')
-                                executed_amount = order_dict.get('executed_amount')
-                                average_price = order_dict.get('average_price')
-                                status = order_dict.get('status')
-                                # 約定通知がONのユーザで約定があった場合
-                                if status == 'FULLY_FILLED' and user.notify_if_filled == 'ON':
-                                    # 約定通知メール
-                                    readable_datetime = datetime.fromtimestamp(int(int(order_dict['ordered_at']) / 1000))
-                                    context = { "user": user, "order_dict": order_dict, 'readable_datetime': readable_datetime }
-                                    subject = get_template('bitbank/mail_template/fill_notice/subject.txt').render(context)
-                                    message = get_template('bitbank/mail_template/fill_notice/message.txt').render(context)
-                                    user.email_user(subject, message)
-                                    logger.info('notice sent to:' + user.email_for_notice)
-                                subj_order = Order.objects.filter(order_id=order_id).get()
-                                subj_order.remaining_amount = remaining_amount
-                                subj_order.executed_amount = executed_amount
-                                subj_order.average_price = average_price
-                                subj_order.status = status
-                                subj_order.save()
+                                ret = prv.get_orders_info(
+                                    pair, # ペア
+                                    list(orders_by_pair) # 注文IDのリスト
+                                )
                             except Exception as e:
-                                logger.error('user:' + user.email + ' pair:' + pair + ' order id: ' + str(orders_by_pair) + ' error: ' + str(e.args))
-        logger.info('completed')
+                                logger.error('user:' + user.email + ' pair: ' + pair + ' order id: ' + str(orders_by_pair) + ' error: ' + str(e.args))
+                                continue
+
+                            for order_dict in ret.get('orders'):
+                                try:
+                                    order_id = order_dict.get('order_id')
+                                    remaining_amount = order_dict.get('remaining_amount')
+                                    executed_amount = order_dict.get('executed_amount')
+                                    average_price = order_dict.get('average_price')
+                                    status = order_dict.get('status')
+                                    # 約定通知がONのユーザで約定があった場合
+                                    if status == 'FULLY_FILLED' and user.notify_if_filled == 'ON':
+                                        # 約定通知メール
+                                        readable_datetime = datetime.fromtimestamp(int(int(order_dict['ordered_at']) / 1000))
+                                        context = { "user": user, "order_dict": order_dict, 'readable_datetime': readable_datetime }
+                                        subject = get_template('bitbank/mail_template/fill_notice/subject.txt').render(context)
+                                        message = get_template('bitbank/mail_template/fill_notice/message.txt').render(context)
+                                        user.email_user(subject, message)
+                                        logger.info('notice sent to:' + user.email_for_notice)
+                                    subj_order = Order.objects.filter(order_id=order_id).get()
+                                    subj_order.remaining_amount = remaining_amount
+                                    subj_order.executed_amount = executed_amount
+                                    subj_order.average_price = average_price
+                                    subj_order.status = status
+                                    subj_order.save()
+                                except Exception as e:
+                                    logger.error('user:' + user.email + ' pair:' + pair + ' order id: ' + str(orders_by_pair) + ' error: ' + str(e.args))
+            logger.info('completed')
--- bitbankproject/bitbank/management/commands/monitor_ticker.py
+++ bitbankproject/bitbank/management/commands/monitor_ticker.py
@@ -22,86 +22,87 @@ class Command(BaseCommand):
     # コマンドが実行された際に呼ばれるメソッド
     def handle(self, *args, **options):
         logger = logging.getLogger('batch_logger')
-        logger.info('started')
-        pub = python_bitbankcc.public()
-        for user in User.objects.all():
-            # API KEYが登録されているユーザのみ処理
-            if user.api_key != "" or user.api_secret_key != "":
-                # キー情報セット
-                try:
-                    prv = python_bitbankcc.private(user.api_key, user.api_secret_key)
-                except Exception as e:
-                    logger.error('User:' + user.email + ' Message: ' + str(e.args))
-                    continue
-
-                # 通貨ペアをイテレーション
-                for pair in Order.PAIR:
-                    # Tickerの取得
+        for i in range(10):
+            logger.info('started')
+            pub = python_bitbankcc.public()
+            for user in User.objects.all():
+                # API KEYが登録されているユーザのみ処理
+                if user.api_key != "" or user.api_secret_key != "":
+                    # キー情報セット
                     try:
-                        ticker_dict = pub.get_ticker(pair)
+                        prv = python_bitbankcc.private(user.api_key, user.api_secret_key)
                     except Exception as e:
-                        logger.error('user:' + user.email + ' pair:' + pair + ' error:' + str(e.args))
+                        logger.error('User:' + user.email + ' Message: ' + str(e.args))
                         continue
-                    
-                    # 通知処理
-                    try:
-                        alerts_by_pair = Alert.objects.filter(pair=pair).filter(is_active=True)
-                        for alert in alerts_by_pair:
-                            if (alert.over_or_under == '以上' and float(ticker_dict.get('buy')) >= alert.threshold) or \
-                                (alert.over_or_under == '以上' and float(ticker_dict.get('buy')) >= alert.threshold):
-                                context = { "user": user, "ticker_dict": ticker_dict, "pair": pair }
-                                subject = get_template('bitbank/mail_template/rate_notice/subject.txt').render(context)
-                                message = get_template('bitbank/mail_template/rate_notice/message.txt').render(context)
-                                user.email_user(subject, message)
-                                logger.info('rate notice sent to:' + user.email_for_notice)
-                    except Exception as e:
-                        logger.error('user:' + user.email + ' pair:' + pair + ' alert:' + alert.pk + ' error:' + str(e.args))
 
-                    # 逆指値の注文取得
-                    stop_market_orders_by_pair = Order.objects.filter(pair=pair).filter(order_type='逆指値').filter(order_id__isnull=True)
-                    
-                    # 各注文を処理
-                    for stop_market_order in stop_market_orders_by_pair:
-                        # 売りの場合
-                        if (stop_market_order.side == 'sell' and float(ticker_dict.get('sell')) <= stop_market_order.price_for_stop) or \
-                            (stop_market_order.side == 'buy' and float(ticker_dict.get('buy')) >= stop_market_order.price_for_stop):
-                            # 成行で売り注文
-                            try:
-                                res_dict = prv.order(
-                                    pair, # ペア
-                                    stop_market_order.price, # 価格
-                                    stop_market_order.start_amount, # 注文枚数
-                                    stop_market_order.side, # 注文サイド
-                                    'market' # 注文タイプ
-                                )
-                                stop_market_order.order_id = res_dict.get('order_id')
-                                stop_market_order.ordered_at = res_dict.get('ordered_at')
-                                stop_market_order.status = res_dict.get('status')     
-                                stop_market_order.save() 
-                            except Exception as e:
-                                logger.error('user:' + user.email + 'pair:' + pair + ' pk:' + stop_market_order.pk + ' error: ' +  str(e.args))
-                                continue
+                    # 通貨ペアをイテレーション
+                    for pair in Order.PAIR:
+                        # Tickerの取得
+                        try:
+                            ticker_dict = pub.get_ticker(pair)
+                        except Exception as e:
+                            logger.error('user:' + user.email + ' pair:' + pair + ' error:' + str(e.args))
+                            continue
+                        
+                        # 通知処理
+                        try:
+                            alerts_by_pair = Alert.objects.filter(pair=pair).filter(is_active=True)
+                            for alert in alerts_by_pair:
+                                if (alert.over_or_under == '以上' and float(ticker_dict.get('buy')) >= alert.threshold) or \
+                                    (alert.over_or_under == '以上' and float(ticker_dict.get('buy')) >= alert.threshold):
+                                    context = { "user": user, "ticker_dict": ticker_dict, "pair": pair }
+                                    subject = get_template('bitbank/mail_template/rate_notice/subject.txt').render(context)
+                                    message = get_template('bitbank/mail_template/rate_notice/message.txt').render(context)
+                                    user.email_user(subject, message)
+                                    logger.info('rate notice sent to:' + user.email_for_notice)
+                        except Exception as e:
+                            logger.error('user:' + user.email + ' pair:' + pair + ' alert:' + alert.pk + ' error:' + str(e.args))
 
-                    # ストップリミットの注文取得
-                    stop_limit_orders_by_pair = Order.objects.filter(pair=pair).filter(order_type='ストップリミット').filter(order_id__isnull=True)
-                    
-                    # 各注文を処理
-                    for stop_limit_order in stop_limit_orders_by_pair:
-                        if (stop_limit_order.side == 'sell' and ticker_dict.get('sell') <= stop_limit_order.price_for_stop) or \
-                            (stop_limit_order.side == 'buy' and ticker_dict.get('buy') >= stop_limit_order.price_for_stop):
-                            try:
-                                res_dict = prv.order(
-                                    pair, # ペア
-                                    stop_limit_order.price, # 価格
-                                    stop_limit_order.start_amount, # 注文枚数
-                                    stop_limit_order.side, # 注文サイド
-                                    'limit' # 注文タイプ
-                                )
-                                stop_limit_order.order_id = res_dict.get('order_id')
-                                stop_limit_order.ordered_at = res_dict.get('ordered_at')
-                                stop_limit_order.status = res_dict.get('status')
-                                stop_limit_order.save()
-                            except:
-                                logger.error('user:' + user.email + 'pair:' + pair + ' pk:' + stop_market_order.pk + ' error: ' +  str(e.args))
-                                continue
-        logger.info('completed')
+                        # 逆指値の注文取得
+                        stop_market_orders_by_pair = Order.objects.filter(pair=pair).filter(order_type='逆指値').filter(order_id__isnull=True)
+                        
+                        # 各注文を処理
+                        for stop_market_order in stop_market_orders_by_pair:
+                            # 売りの場合
+                            if (stop_market_order.side == 'sell' and float(ticker_dict.get('sell')) <= stop_market_order.price_for_stop) or \
+                                (stop_market_order.side == 'buy' and float(ticker_dict.get('buy')) >= stop_market_order.price_for_stop):
+                                # 成行で売り注文
+                                try:
+                                    res_dict = prv.order(
+                                        pair, # ペア
+                                        stop_market_order.price, # 価格
+                                        stop_market_order.start_amount, # 注文枚数
+                                        stop_market_order.side, # 注文サイド
+                                        'market' # 注文タイプ
+                                    )
+                                    stop_market_order.order_id = res_dict.get('order_id')
+                                    stop_market_order.ordered_at = res_dict.get('ordered_at')
+                                    stop_market_order.status = res_dict.get('status')     
+                                    stop_market_order.save() 
+                                except Exception as e:
+                                    logger.error('user:' + user.email + 'pair:' + pair + ' pk:' + stop_market_order.pk + ' error: ' +  str(e.args))
+                                    continue
+
+                        # ストップリミットの注文取得
+                        stop_limit_orders_by_pair = Order.objects.filter(pair=pair).filter(order_type='ストップリミット').filter(order_id__isnull=True)
+                        
+                        # 各注文を処理
+                        for stop_limit_order in stop_limit_orders_by_pair:
+                            if (stop_limit_order.side == 'sell' and ticker_dict.get('sell') <= stop_limit_order.price_for_stop) or \
+                                (stop_limit_order.side == 'buy' and ticker_dict.get('buy') >= stop_limit_order.price_for_stop):
+                                try:
+                                    res_dict = prv.order(
+                                        pair, # ペア
+                                        stop_limit_order.price, # 価格
+                                        stop_limit_order.start_amount, # 注文枚数
+                                        stop_limit_order.side, # 注文サイド
+                                        'limit' # 注文タイプ
+                                    )
+                                    stop_limit_order.order_id = res_dict.get('order_id')
+                                    stop_limit_order.ordered_at = res_dict.get('ordered_at')
+                                    stop_limit_order.status = res_dict.get('status')
+                                    stop_limit_order.save()
+                                except:
+                                    logger.error('user:' + user.email + 'pair:' + pair + ' pk:' + stop_market_order.pk + ' error: ' +  str(e.args))
+                                    continue
+            logger.info('completed')
